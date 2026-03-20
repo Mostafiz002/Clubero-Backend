d7eb44bb1294ef559dbb768512581699f01b95d4
Mostafiz002 <rtogether498@gmail.com>
2026-03-20 14:48:07 +0600

    chore: add payment route

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -6,6 +6,7 @@ import { EventRegistrationRoutes } from "../modules/Event_Registration/eventRegi
 import { MembershipRoutes } from "../modules/membership/membership.route";
 import { DashboardRoutes } from "../modules/dashboard/dashboard.route";
 import { AdminRoutes } from "../modules/admin/admin.routes";
+import { PaymentRoutes } from "../modules/payments/payment.routes";
 
 const router = Router();
 
@@ -14,7 +15,8 @@ router.use("/clubs", ClubRoutes);
 router.use("/events", EventRoutes);
 router.use("/eventRegistration", EventRegistrationRoutes);
 router.use("/membership", MembershipRoutes);
-router.use("/dashboard",DashboardRoutes);
+router.use("/dashboard", DashboardRoutes);
 router.use("/admin", AdminRoutes);
+router.use("", PaymentRoutes);
 
 export default router;
